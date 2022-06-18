d23d4f210b1fde448170940248c6392fd83b27bf
yunnix22 <aboomarmoshab22@gmail.com>
2022-06-18 14:43:47 +0700

    feat: The app can display contact from data

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,17 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({ data }) => {
+    const { name, phone, email, photo } = data;
     return (
-        <></>
+        <div className="contact">
+            <img className="contact-img" src={photo} alt={`${name}`} />
+            <div className="detail">
+                <h3>{name}</h3>
+                <p>{phone}</p>
+                <p>{email}</p>
+            </div>
+        </div>
     )
 }
 
